4ec388346fbac3cb67c23571c570501949601459
shai <shimi22_shm@hotmail.com>
2021-10-14 01:33:33 +0300

    finish function updateUser and deleteUser

--- server2/models/usersBL.js
+++ server2/models/usersBL.js
@@ -41,57 +41,99 @@ const getUserByID = async (userID) => {
 
 const addUser = async (userData) => {
   return new Promise((resolve, reject) => {
-    let user = new usersModel({ username: userData.username, password: userData.password });
+    let user = new usersModel({
+      username: userData.username,
+      password: userData.password,
+    });
 
-    // Write Username To DataBase 
+    // Write Username To DataBase
     user.save(async (err, data) => {
-        if (err) {
-            reject(err);
-        }
-        
-        // Write User Info To Users.json File
-        let allUsers = await jsonDAL.readJsonFile("users.json");
-        allUsers.users.push({
-            _id: data._id.toString(),
-            first_name: userData.first_name,
-            last_name: userData.last_name,
-            created_date: "12/04/21",
-            session_timeout: userData.session_timeout,
-        });
-        await jsonDAL.writeJsonFile("users.json", allUsers)
+      if (err) {
+        reject(err);
+      }
 
-        // Write User's Permissions Data To permissions.json File
-        let allUsers2 = await jsonDAL.readJsonFile("permissions.json");
-        allUsers2.permissions.push({
-            _id: data._id.toString(),
-            permissions: userData.permissions,
-        })
-        await jsonDAL.writeJsonFile("permissions.json", allUsers2)
+      // Write User Info To Users.json File
+      let allUsers = await jsonDAL.readJsonFile("users.json");
+      allUsers.users.push({
+        _id: data._id.toString(),
+        first_name: userData.first_name,
+        last_name: userData.last_name,
+        created_date: "12/04/21",
+        session_timeout: userData.session_timeout,
+      });
+      await jsonDAL.writeJsonFile("users.json", allUsers);
 
-        
-      resolve({ _id: data._id.toString(), ...userData});
+      // Write User's Permissions To permissions.json File
+      let allUsers2 = await jsonDAL.readJsonFile("permissions.json");
+      allUsers2.permissions.push({
+        _id: data._id.toString(),
+        permissions: userData.permissions,
+      });
+      await jsonDAL.writeJsonFile("permissions.json", allUsers2);
+
+      // Returning The New User Data With New ID
+      resolve({ _id: data._id.toString(), ...userData });
     });
   });
 };
 
 const updateUser = async (userID, userData) => {
   return new Promise((resolve, reject) => {
-    console.log(userData);
-    usersModel.findByIdAndUpdate(userID, userData, (err, data) => {
+    usersModel.findByIdAndUpdate(userID, userData, async (err, data) => {
       if (err) {
         reject(err);
       }
-      resolve(data);
+
+      // Update User Info in Users.json File
+
+      let allUsers = await jsonDAL.readJsonFile("users.json");
+      let index = allUsers.users.findIndex((user) => user._id == userID);
+      allUsers.users[index] = {
+        _id: userID,
+        first_name: userData.first_name,
+        last_name: userData.last_name,
+        created_data: userData.created_date,
+        session_timeout: userData.session_timeout,
+      };
+      await jsonDAL.writeJsonFile("users.json", allUsers);
+
+      // Update User's Permissions in permissions.json File
+      let allPermissions = await jsonDAL.readJsonFile("permissions.json");
+      let index2 = allPermissions.permissions.findIndex(
+        (item) => item._id == userID
+      );
+      allPermissions.permissions[index2] = {
+        _id: userID,
+        permissions: userData.permissions,
+      };
+      await jsonDAL.writeJsonFile("permissions.json", allPermissions);
+
+      // Returning The Updated User Data
+      resolve({ _id: userID, ...userData });
     });
   });
 };
 
 const deleteUser = async (userID) => {
   return new Promise((resolve, reject) => {
-    usersModel.findByIdAndDelete(userID, (err, data) => {
+    usersModel.findByIdAndDelete(userID, async (err, data) => {
       if (err) {
         reject(err);
       }
+
+      let allUsers = await jsonDAL.readJsonFile("users.json");
+      allUsers = allUsers.users.filter((user) => user._id !== userID);
+      await jsonDAL.writeJsonFile("users.json", { users: allUsers });
+
+      let allPermissions = await jsonDAL.readJsonFile("permissions.json");
+      allPermissions = allPermissions.permissions.filter(
+        (item) => item._id !== userID
+      );
+      await jsonDAL.writeJsonFile("permissions.json", {
+        permissions: allPermissions,
+      });
+
+      // Returning Username Of Delted User
       resolve(data);
     });
   });
